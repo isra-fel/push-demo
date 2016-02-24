f301fc5379f80965aa75b2e83282899fb91bd784
Yeming Liu <liuym13@lenovo.com>
2016-02-24 10:00:27 +0800

    Use path@nodejs to join files

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var express = require('express');
 var app = express();
 var handlePush = require('./pushts');
 var morgan = require('morgan');
+var path = require('path');
 app.use(morgan('dev'));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, '/public')));
 handlePush(app, '/');
 app.listen(8080, function () {
     console.log('app started @8080');
