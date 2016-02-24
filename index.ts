1f810a256304cefa3a982a3d2e3e1d8c3c16b035
Yeming Liu <liuym13@lenovo.com>
2016-02-24 10:01:36 +0800

    Use path@nodejs to join directories

--- index.ts
+++ index.ts
@@ -2,9 +2,10 @@ import * as express from 'express';
 var app = express();
 import handlePush = require('./pushts');
 import morgan = require('morgan');
+import path = require('path');
 
 app.use(morgan('dev'));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, '/public')));
 handlePush(app, '/');
 
 app.listen(8080, function () {
